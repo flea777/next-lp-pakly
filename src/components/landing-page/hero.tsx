7ad4e8279ba0261bdd04b70a341dc16e4544ba59
Pedro Bim <pedrodinizz2@outlook.com>
2025-10-15 01:35:43 -0300

    Ajustes visuais gerais, finalização da seção presentation

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
     const texts = ['notificações inteligentes.', 'controle de encomendas.', 'assinaturas digitais.', 'relatórios em tempo real.']
 
     return (
-        <section className="flex flex-col gap-8 justify-center items-center min-h-[380px] pt-8">
+        <section className="flex flex-col gap-8 justify-center items-center min-h-[380px] py-20">
             <div className="flex flex-col gap-8 px-12 items-center">
                 <h2 className={`text-7xl font-bold text-center text-primary max-w-3/4 ${lora.className}`}>Simplifique a <strong className="text-foreground italic">gestão de encomendas</strong> do seu condomínio</h2>
                 <div className="text-center text-lg">
