5f732c10787102e0b11157c42e921d4a7fcf3b73
Pedro Bim <pedrodinizz2@outlook.com>
2025-10-21 02:10:08 -0300

    Mudança na paleta de cores

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -1,6 +1,6 @@
+import { ArrowRight } from 'lucide-react'
 import { dynaPuff } from '@/lib/fonts'
 import { Button } from '../commons/ui/button'
-import { ArrowRight } from 'lucide-react'
 
 interface NavBarProps {
 	title: string
@@ -9,12 +9,6 @@ interface NavBarProps {
 }
 
 export function Header() {
-	const navBarData: NavBarProps[] = [
-		{ title: 'Início', href: '#' },
-		{ title: 'Serviços', href: '#' },
-		{ title: 'Contato', href: '#' },
-	]
-
 	return (
 		<header className='flex py-6 px-16'>
 			<div className='flex justify-between rounded-2xl w-7xl p-4 bg-sidebar-primary'>
@@ -23,17 +17,6 @@ export function Header() {
 				>
 					pakly!
 				</h2>
-				<div className='flex gap-4'>
-					{navBarData.map((item, idx) => (
-						<Button
-							key={idx}
-							variant={'ghost'}
-							className='text-lg text-primary-foreground'
-						>
-							{item.title}
-						</Button>
-					))}
-				</div>
 				<div>
 					<Button
 						variant={'default'}
